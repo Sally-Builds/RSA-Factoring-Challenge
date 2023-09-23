3d92286da37f7ca3434fd7cbd6effd8a32d748ba
Sally-Builds <uzoagulujoshua@yahoo.com>
2023-09-23 03:24:56 -0700

    rsa initial

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 1024
+
+void factorize(char *token);
+int main (int argc, char *argv[])
+{
+        FILE *file;
+        char line[MAX_LINE_LENGTH];
+
+        if (argc != 2)
+        {
+                fprintf(stderr, "Usage: factors <file>");
+                exit(EXIT_FAILURE);
+        }
+
+        file = fopen(argv[1], "r");
+
+        if (!file)
+        {
+                fprintf(stderr, "File not found");
+                exit(EXIT_FAILURE);
+        }
+
+	while (fgets(line, MAX_LINE_LENGTH, file))
+	{
+		char *token = strtok(line, " \t\r\n");
+		factorize(token);
+	}
+	
+	return (0);
+}
+
+void factorize(char *token)
+{
+	long long int  value;
+	long long int half;
+	long long int i;
+
+	if (!token)
+	{
+		fprintf(stderr, "Error");
+	}
+
+	value = atoll(token);	
+	half = value / 2;
+
+	for (i = 2; i <= half; i++)
+	{
+		if (value % i == 0)
+		{
+			long long int q = value / i;
+			printf("%lld=%lld*%lld\n", value, q, i);
+			return;
+		}
+	}
+	
+	return;
+}
